3188b646b15021c270f2a53dde4518842703fb83
Bruno Coimbra <bbcoimbra@gmail.com>
2015-10-13 19:20:38 -0300

    fix migration

--- db/migrate/20151013210607_create_issue_message_ids.rb
+++ db/migrate/20151013210607_create_issue_message_ids.rb
@@ -1,11 +1,11 @@
 class CreateIssueMessageIds < ActiveRecord::Migration
   def change
     create_table :issue_message_ids do |t|
-      t.references :issue, index: true, foreign_key: true
+      t.references :issue, index: true
       t.string :message_id
 
       t.timestamps null: false
     end
-    add_index :issue_message_ids, :message_id, unique: true
+    add_index :issue_message_ids, :message_id
   end
 end
